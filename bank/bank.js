19791148c5c14f39c1681432b42aed4117a4b3ec
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-02-11 12:56:06 +0600

    deposit-button

--- bank/bank.js
+++ bank/bank.js
@@ -1,6 +1,18 @@
-document.getElementById('submit-button').addEventListener('click', function(
+document.getElementById('submit-button').addEventListener('click', function(){ 
+ const emailField = document.getElementById('user-email');
+ const userEmail = emailField.value;
 
-){ document.getElementById('user-email')
-const userEmail = userEmailFild 
+ const passwordField = document.getElementById('user-password');
+ const userPassword = passwordField.value;
+ console.log(userPassword);
+
+if (userEmail == 'itazmir@gmail.com' &&  userPassword == 'tazmir'){
+    window.location.href = 'new-bank.html';
+}
+});
+
+document.getElementById('deposit-button').addEventListener
+('click', function(){
+    console.log('deposit button clicked')
 
 })
