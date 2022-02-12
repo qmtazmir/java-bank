26a9deb617fce11db11b5339a8b1af63ec24c3de
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-02-12 19:22:32 +0600

    updat-blance

--- bank/banking.js
+++ bank/banking.js
@@ -3,10 +3,31 @@ document.getElementById('deposit-button').addEventListener
 ('click', function(){
 // get the amount deposited
 
-    const depositAmount = document.getElementById('deposit-amount');
-    const depositInput = depositAmount.value;
-    console.log(depositInput);
+
+//update deposit
+
+    const depositAmount = document.getElementById('deposit-input');
+
+    const newDepositInputText = depositAmount.value;
+    const newDepositInput = parseFloat(newDepositInputText);
 
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositInput;
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText)
+    const newDepositTotal = previousDepositAmount + newDepositInput;
+  
+    depositTotal.innerText = newDepositTotal;
+    
+    
+
+
+    // updat-blance-----
+    const blanceTotal = document.getElementById('updat-blance');
+    const blanceTotalText = blanceTotal.innerText;
+    const previousBlanceTotal= parseFloat(blanceTotalText);
+    const newBlanceTotal = previousBlanceTotal + newDepositInput;
+    blanceTotal.innerText = newBlanceTotal;
+
+
+    depositAmount.value = '';
 })
