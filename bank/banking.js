b3f27b69fef0b1cf4ca394ce33fac77d17faef0f
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-02-12 19:55:59 +0600

    new-blance

--- bank/banking.js
+++ bank/banking.js
@@ -1,33 +1,28 @@
 //handle deposit button event
-document.getElementById('deposit-button').addEventListener
-('click', function(){
-// get the amount deposited
+const depositButton = document.getElementById("deposit-button");
 
+function handleDeposit() {
+  const depositInput = document.getElementById("deposit-input");
 
-//update deposit
+  const newDepositAmount = parseFloat(depositInput.value);
 
-    const depositAmount = document.getElementById('deposit-input');
+  const depositTotal = document.getElementById("deposit-total");
 
-    const newDepositInputText = depositAmount.value;
-    const newDepositInput = parseFloat(newDepositInputText);
+  const previousDepositAmount = parseFloat(depositTotal.innerText);
 
-    const depositTotal = document.getElementById('deposit-total');
-    const previousDepositText = depositTotal.innerText;
-    const previousDepositAmount = parseFloat(previousDepositText)
-    const newDepositTotal = previousDepositAmount + newDepositInput;
-  
-    depositTotal.innerText = newDepositTotal;
-    
-    
+  const newDepositTotal = previousDepositAmount + newDepositAmount;
 
+  depositTotal.innerText = newDepositTotal;
 
-    // updat-blance-----
-    const blanceTotal = document.getElementById('updat-blance');
-    const blanceTotalText = blanceTotal.innerText;
-    const previousBlanceTotal= parseFloat(blanceTotalText);
-    const newBlanceTotal = previousBlanceTotal + newDepositInput;
-    blanceTotal.innerText = newBlanceTotal;
+  // updat-blance-----
+  const blanceTotal = document.getElementById("updat-blance");
+  const previousBlanceTotal = parseFloat(blanceTotal.innerText);
 
+  const newBlanceTotal = previousBlanceTotal + newDepositAmount;
 
-    depositAmount.value = '';
-})
+  blanceTotal.innerText = newBlanceTotal;
+
+  depositInput.value = "";
+}
+
+depositButton.addEventListener("click", handleDeposit);
